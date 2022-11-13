b8c3a2a0974c3c8e0c6f6fefb1969ff7651462fc
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-11-13 22:02:36 +0530

    added future ideas

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function render() {
     document.getElementById("container").innerHTML = currentDog.getDogHtml()
 }
 
-getNewDog()
-getNewDog()
-render()
+// Create conditional logic on render - 
+// if is swiped is true, and if liked is true, display liked and have set time out lock
+// if is liked is false, display disliked and set time out
+// if swiped is false do nothing
+//render
+
+// add a single page event listener to handle button id clicks
+// data is passed according to button id, and a handle click function is called, which handles clicks, logic and render method.
+
+// if array is empty, display no more matches in main
